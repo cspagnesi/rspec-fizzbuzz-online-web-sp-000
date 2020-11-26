a4e1d8862ec89cb0638661aab1bfe8b0df6ed384
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-11-26 17:05:29 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,10 +10,10 @@ describe "fizzbuzz" do
   it 'returns "Buzz" when the number is divisible by 5'  do
     fizz_5.to = "Buzz"
 
-    expect(fizz_5).to eq("FizzBuzz")
+    expect(fizz_5).to eq("Buzz")
   end
-  it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
-    fizz_15 = "FizzBuzz"
+  it 'returns "Buzz" when the number is divisible by 3 and 5' do
+    fizz_15 = "Buzz"
 
     expect(fizz_15).to eq("FizzBuzz")
   end
